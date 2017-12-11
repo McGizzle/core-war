6ff7a5c4942097d02b4256b35eae9e7ccba194a0
Sean McGroarty <mcgroas@tcd.ie>
2017-12-11 22:59:49 +0000

    added: to data field

--- src/MARS.hs
+++ src/MARS.hs
@@ -3,26 +3,45 @@ import Control.Monad
 import Control.Monad.Reader
 import Control.Concurrent
 import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM.TQueue
+import Data.Time
+import Data.Map as Map
 import Parser
 
+type AppT = ReaderT Data IO
+type Memory = TVar (Map Int Int)
 data Data = Data {
-  prog :: Program,
-  queue :: TQueue ThreadId
+  prog         :: Program,
+  aliveThreads :: TVar [ThreadId],
+  queue        :: TQueue ThreadId,
+  memory       :: Memory
 }
 
-type AppT = ReaderT Data IO
+run :: [Program] -> UTCTime -> IO ()
+run progs endTime = do
+  mem <- liftIO $ atomically $ newTVar Map.empty 
+  queue <- liftIO $ atomically newTQueue
+  threads <- liftIO $ atomically $ newTVar [] 
+  ids <- mapM (\ p -> forkIO $ runThread p queue mem threads) progs
+  liftIO $ atomically $ writeTVar threads ids
+  addToQueue threads queue endTime
 
-run :: [Program] -> IO [ThreadId]
-run progs = do
-  queue <- atomically $ newTQueue
-  mapM (\ p -> forkIO $ runThread p queue) progs  
+addToQueue :: TVar [ThreadId] -> TQueue ThreadId -> UTCTime -> IO ()
+addToQueue threads queue endTime = do
+  ids <- atomically $ readTVar threads
+  mapM_ (liftIO . atomically . writeTQueue queue) ids
+  time <- getCurrentTime
+  unless (time >= endTime) $ addToQueue threads queue endTime 
+  return ()
 
-runThread :: Program -> TQueue ThreadId -> IO () 
-runThread p q = do
+runThread :: Program -> TQueue ThreadId -> Memory -> TVar [ThreadId] -> IO () 
+runThread p q mem threads = do
   let info = Data {
       prog = p,
-      queue = q
+      queue = q,
+      memory = mem,
+      aliveThreads = threads
   }
   runReaderT runProg info
   return ()
@@ -30,19 +49,19 @@ runThread p q = do
 runProg :: AppT ()
 runProg = do
   p <- asks prog
-  execute p
+  loopProg p
 
-execute :: Program -> AppT ()
-execute [] = runProg
-execute (x:xs) = do
+loopProg :: Program -> AppT ()
+loopProg [] = runProg
+loopProg (x:xs) = do
   checkQueue
   liftIO $ print $ show x
-  execute xs
+  loopProg xs
 
 checkQueue :: AppT ()
 checkQueue = do
   q <- asks queue
   id <- liftIO $ atomically $ peekTQueue q
-  myId <- liftIO $ myThreadId
-  unless (myId == id) $ checkQueue
+  myId <- liftIO myThreadId
+  unless (myId == id) checkQueue
   return ()
