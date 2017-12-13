8e8080ae3e23a07ffa659b7bd2adc504219a3add
Sean McGroarty <mcgroas@tcd.ie>
2017-12-13 16:42:23 +0000

    added: all instructions

--- src/Instructions.hs
+++ src/Instructions.hs
@@ -3,6 +3,7 @@ module Instructions where
 
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Writer
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TVar
@@ -14,15 +15,18 @@ import Parser
 runInstruction :: AppT Bool
 runInstruction = do
   ins <- nextInstruction
-  lift $ lift $ put ins 
-  matchIns ins
+  lift $ lift $ put ins
+  lift $ lift $ lift $ tell $ "Executing Instruction: " ++ show ins 
+  res <- matchIns ins
+  endTask
+  return res
 
 nextInstruction :: AppT Instruction
 nextInstruction = do
-  pc <- lift get
+  pc <- getPc
   ins <- readMemory (Map.lookup pc)
   return $ fromJust ins
- 
+
 readMemory :: (MonadReader MarsData m, MonadIO m) => (Map Int Instruction -> b) -> m b
 readMemory op = do
   mem <- asks memory
@@ -35,39 +39,50 @@ writeMemory op = do
   return ()
 
 matchIns :: Instruction -> AppT Bool
-matchIns I0          = return False
+matchIns I0          = return True
 matchIns (I1 op)     = matchOp0 op
 matchIns (I2 op a)   = matchOp1 op a
 matchIns (I3 op a b) = matchOp2 op a b
 
 matchOp0 DAT = return False
-matchOp1 DAT a = return False
-matchOp1 SPL a = return False
+matchOp1 DAT _ = return False
+matchOp1 SPL a = spl a 
 matchOp1 JMP a = jmp a 
 matchOp2 MOV a b = mov a b
-matchOp2 CMP a b = return False
-matchOp2 ADD a b = return False
-matchOp2 SUB a b = return False
+matchOp2 CMP a b = cmp a b
+matchOp2 ADD a b = add a b 
+matchOp2 SUB a b = sub a b
 matchOp2 JMZ a b = jmz a b
 matchOp2 DJN a b = djn a b
 
 matchField :: Field -> AppT Int
 matchField (Direct a)  = do
-  pc <- lift get
+  pc <- getPc
   return $ pc + a
 matchField (Indirect a) = do 
-  pc <- lift get
+  pc <- getPc
   nextIns <- readMemory $ Map.lookup (pc + a)
   nextB <- getBField (fromJust nextIns) 
   curIns <- lift $ lift get
   curB <- getBField curIns
   return (nextB + curB)
-matchField (Immediate a)     = return a 
+matchField (Immediate a)     = matchField $ Direct a 
 matchField (AutoDecrement a) = matchField $ Indirect (a-1) 
 
 getBField :: Instruction -> AppT Int
-getBField (I3 _ _ b)  = return 0   
-getBField I0          = return 0
+getBField (I3 _ _ b) = matchField b   
+getBField (I2 _ b)   = matchField b
+getBField _          = return 0
+
+--updateBField :: Int -> Instruction -> AppT Instruction
+updateBField mod (I3 x y b) = return $ I3 x y (modField mod b)
+updateBField mod (I2 x b) = return $ I2 x (modField mod b) 
+
+modField :: (t -> a) -> AddrMode t -> AddrMode a
+modField mod (Direct a) = Direct (mod a)
+modField mod (Indirect a) = Indirect (mod a)
+modField mod (Immediate a) = Immediate (mod a)
+modField mod (AutoDecrement a) = AutoDecrement (mod a)
 
 mov :: Field -> Field -> AppT Bool
 mov a b = do 
@@ -75,15 +90,43 @@ mov a b = do
   b' <- matchField b
   aConts <- readMemory $ Map.lookup a'
   writeMemory $ Map.insert b' (fromJust aConts) 
+  updatePc
   return True
 
 add :: Field -> Field -> AppT Bool
-add a b = return True
+add = replace (+) 
+
+sub :: Field -> Field -> AppT Bool
+sub = replace (-)
+
+replace mod a b = do
+  a' <- matchField a
+  b' <- matchField b
+  ins <- readMemory $ Map.lookup b'
+  newIns <- updateBField (mod a') (fromJust ins)
+  updatePc
+  return True
+
+cmp :: Field -> Field -> AppT Bool
+cmp a b = do
+  a' <- matchField a
+  b' <- matchField b
+  pc <- getPc
+  when (a' /= b') updatePc
+  updatePc 
+  return True
 
 jmp :: Field -> AppT Bool
 jmp a = do
-  addr <- matchField a
-  lift $ put addr
+  a' <- matchField a
+  putPc a'
+  return True
+
+spl :: Field -> AppT Bool
+spl a = do
+  a' <- matchField a
+  addTask a'
+  updatePc
   return True
 
 jmz :: Field -> Field -> AppT Bool
@@ -98,8 +141,8 @@ djn = cndJmp (\ x -> (x - 1) == 0)
 cndJmp :: (Int -> Bool) -> Field -> Field -> AppT Bool
 cndJmp cnd a b = do
   b' <- matchField b
-  when (cnd b') $ do
+  if cnd b' then do
     a' <- matchField a
-    lift $ put a'
+    putPc a'
+  else updatePc
   return True
-
